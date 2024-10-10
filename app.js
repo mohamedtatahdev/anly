f0b62a1970e858119b0cbeae91b581ba64229d66
mohamedtatahdev <mohamed.tatah69@gmail.com>
2024-10-10 20:15:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+require('dotenv').config({path: './config/.env'});
+const index = require('./routes');
+const ejsLayouts = require('express-ejs-layouts');
+
+app.use(ejsLayouts);
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layout');  // layout.ejs sera le fichier de layout par défaut
+
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(index);
+
+app.listen(process.env.PORT);
